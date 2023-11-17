cdb3f8906c55093b419fbb7a9398ab08d6677690
Muhammet Yağız <myagiz24@gmail.com>
2023-11-17 23:33:03 +0300

    User service and layers added.

---
+++ Entities/DTOs/CreateUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class CreateUserDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string EmailAddress { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int RoleId { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("CreateUserAsync")]
+        public async Task<IActionResult> CreateUserAsync(CreateUserDto model)
+        {
+            var result = await _userService.CreateUserAsync(model);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
